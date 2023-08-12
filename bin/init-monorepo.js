b0f0e0db4f880e76d027198d7569a2a001c34904
Santiago <fsgreco@fastmail.com>
2023-08-12 13:12:17 +0200

    Add commands and options and improve prompts

--- bin/init-monorepo.js
+++ bin/init-monorepo.js
@@ -1,14 +1,18 @@
 #! /usr/bin/env node
+import fs from 'node:fs/promises'
 import inquirer from "inquirer"
 import ora from 'ora'
 import { setNpmScript, spawnAsync } from "../utils/functions.js"
 import { createPackageJson, initTokens, initFrontendOfChoice, initBackendOfChoice } from "../helpers/scaffold-functions.js"
-
+import { options, commands } from "../helpers/arguments.js"
+import { checkmark, inBlueBold, inGoldBold, inGreenBold, printNewLine, printSeparator } from "../utils/prints.js"
 
 // TODO if argument is --default or -d do not ask any question
-// TODO if argument is -w or --workspace-choice ask for workspaces 
-// TODO if argument is -i or --interactive ask questions for every package
-// TODO scegliere se installare e ultimare lo scaffolding o se fare solo scaffolding iniziale.
+const choosesDefault = options.default 
+// The first argument will be the project name. or --project or -p also
+const chosenDir =  options.project || commands[0] 
+const chosenBackend = options.backend
+const chosenFrontend = options.frontend
 
 
 // TODO - add gitignore 
@@ -32,6 +36,12 @@ child_process.spawn('docker', ['info'])
 */
 
 // The first argument will be the project name. or --project or -p also
+if (chosenDir) {
+	const directory = options.project || commands[0]
+	console.log(`Hai scelto ${ inBlueBold( directory ) } - creo directory...\n`)
+	await fs.mkdir(directory, { recursive: true })
+	process.chdir( directory )
+}
 
 
 let workspaces = {
@@ -43,12 +53,12 @@ let firstRound = [
 	{ 
 		type: 'confirm', 
 		name: 'change_workspaces', 
-		message: "Default workspaces will be " + Object.values(workspaces) +`
-	.
-	├── ${Object.values(workspaces)[0]}/
+		message: "Default workspaces will be " + inGoldBold( Object.values(workspaces)) +`
+	${options.project || commands[0] || '.'}
+	├── ${inGoldBold(Object.values(workspaces)[0])}/
 	│   ├── backend
 	│   └── frontend
-	├── ${Object.values(workspaces)[1]}/
+	├── ${inGoldBold(Object.values(workspaces)[1])}/
 	│   └── tokens
 	└── package.json
 
@@ -64,53 +74,110 @@ if (answers.change_workspaces === false ) {
 	workspaces.secondary = secondRound.secondary
 	console.info(`Nice, this will be the structure: 
 	.
-	├── ${secondRound.main}/
+	├── ${ inBlueBold(secondRound.main) }/
 	│   ├── backend
 	│   └── frontend
-	├── ${secondRound.secondary}/
+	├── ${ inBlueBold(secondRound.secondary) }/
 	│   └── tokens
 	└── package.json
 	
 Proceeding...\n`)
 }
 
+printNewLine()
+
+// Choose your backend ['laravel', 'django']
+let backends = ['laravel', 'django', 'none']
+let backend 
+if (chosenBackend && backends.includes(chosenBackend)) {
+	backend = {}
+	backend.choice = chosenBackend
+} else {
+	backend = await inquirer.prompt({type: 'list', name: 'choice', message: 'Choose your backend:', choices: backends, default: 'django'})
+}
+
+// Choose your frontend ['vanilla', 'react', 'vue', 'svelte', 'solid', 'quick']
+let frontends = ['vanilla', 'react', 'vue', 'svelte', 'solid', 'qwik', 'preact', 'lit', 'none']
+let frontend 
+if (chosenFrontend && frontends.includes(chosenFrontend)) {
+	frontend = {}
+	frontend.choice = chosenFrontend
+} else {
+	frontend = await inquirer.prompt({type: 'list', name: 'choice', message: 'Choose your frontend:', choices: frontends, default: 'react'})
+}
+
+
 let spinner = ora()
 
 try {
 
-	spinner.start(`Scaffolding main package.json - monorepo with ${Object.values(workspaces)} workspaces.`)
+	spinner.start(`Scaffolding ${inGreenBold('main package.json')} - monorepo with ${Object.values(workspaces)} workspaces.`)
 	await createPackageJson({ workspaces: Object.values(workspaces) })
-	spinner.succeed(`Created main package.json - monorepo with workspaces: ${Object.values(workspaces)}.`)
+	spinner.succeed(`Created ${inGreenBold('main package.json')} - monorepo with workspaces: ${ inGoldBold( Object.values(workspaces) )}.`)
 
 	spinner.start(`Scaffolding Tokens package inside ${workspaces.secondary} workspace`)
 	await initTokens( workspaces.secondary, 'tokens', '@fsg')
-	spinner.succeed(`Created Tokens library.`)
+	spinner.succeed(`Created Tokens library.\n`)
 
-	// Choose your backend ['laravel', 'django']
-	let backends = ['laravel', 'django']
-	let backend = await inquirer.prompt({type: 'list', name: 'choice', message: 'Choose your backend:', choices: backends, default: 'django'})
-	
-	spinner.start(`Scaffolding ${backend.choice} app inside ${workspaces.main} workspace`)
-	await initBackendOfChoice( {workspace: workspaces.main, choice: backend.choice } )
-	spinner.succeed(`Created ${backend.choice} package.`)
+	if (backend.choice !== 'none') {
+		spinner.start(`Scaffolding ${backend.choice} app inside ${workspaces.main} workspace`)
+		await initBackendOfChoice( {workspace: workspaces.main, choice: backend.choice } )
+		spinner.succeed(`Created ${inGreenBold( backend.choice )} package.\n`)
+	}
 
-	// Choose your frontend ['vanilla', 'react', 'vue', 'svelte', 'solid', 'quick']
-	let frontends = ['vanilla', 'react', 'vue', 'svelte', 'solid', 'qwik', 'preact', 'lit']
-	let frontend = await inquirer.prompt({type: 'list', name: 'choice', message: 'Choose your frontend:', choices: frontends, default: 'react'})
-	
-	spinner.start(`Scaffolding ${frontend.choice} frontend inside ${workspaces.main} workspace`)
-	await initFrontendOfChoice( {workspace: workspaces.main, template: frontend.choice } )
-	spinner.succeed(`Created ${frontend.choice} frontend package (with Vite).`)
+	if (frontend.choice !== 'none') {
+		spinner.start(`Scaffolding ${frontend.choice} frontend inside ${workspaces.main} workspace`)
+		await initFrontendOfChoice( {workspace: workspaces.main, template: frontend.choice } )
+		spinner.succeed(`Created ${inGreenBold( frontend.choice) } frontend package (with Vite).\n`)
+	}
 
-	// main scaffolding process is done. Do you want to install depencencies or you'll do it by hand
-	spinner.start('Installing dependencies...')
-	await spawnAsync('npm', ['install'])
-	await setNpmScript({ name: 'start', cmd: 'npm run start:backend & npm run start:frontend' })
-	spinner.succeed('Initialized project and installed dependencies.')
+	if ( !(frontend.choice === 'none' && backend.choice === 'none')) {
+		// main scaffolding process is done. Do you want to install depencencies or you'll do it by hand
+		let installDeps = await inquirer.prompt({ 
+			type: 'confirm', 
+			name: 'answer', 
+			message: "Main scaffolding process is done. Do you want to install depencencies right away?"
+		})
 
-	console.info(`Everything done. Now simply run 'npm start' and happy development 🚀`)
+		if (installDeps.answer === true) {
+			spinner.start('Installing dependencies...')
+			await spawnAsync('npm', ['install'])
+			await setNpmScript({ name: 'start', cmd: 'npm run start:backend & npm run start:frontend' })
+			spinner.succeed(`Initialized project and installed dependencies.`)
+		}
+	}
+
+	printSeparator()
+	console.info(`${checkmark} Everything done. Now simply run 'npm start' and happy development 🚀`)
 
 } catch (error) {
 	console.error(error)
 	process.exit(1)
 }
+
+
+
+
+
+
+
+
+
+/* TODOS */
+
+// TODO - add gitignore 
+
+// Add control to stablish if user has the binary and it works (check the exit status)
+/* So first run the `command -v` and then also some unuseful command with > /dev/null 2>&1 , if result is not 0 then it's not ok
+
+child_process.spawn('docker', ['info'])
+	.on('exit', c => console.log('child exit code (spawn)', c))
+
+	also check what happens if you use stdio 'inherit' with spawn it should output result on terminal, otherwise it will be silent.
+
+	metti sta cosa del spawn nel how to - se gli dai un terzo argomento `{stdio: 'inherit'}` lui farà vedere l'output nel terminale (cioè nel processo sopra quindi quello primario):
+	```js
+	let cp = require('node:child_process')
+	cp.spawn('docker', ['info' ], {stdio: 'inherit'})
+	```
+*/
