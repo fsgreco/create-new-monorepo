49c5d87d2ef115cdae55df1dadc2b03c405af5c5
Santiago <fsgreco@fastmail.com>
2023-08-12 13:10:07 +0200

    Prepare new options and arguments

--- helpers/arguments.js
+++ helpers/arguments.js
@@ -1,8 +1,11 @@
 import { parseArgs } from "node:util"
+import { printNewLine, printSeparator } from "../utils/prints.js"
 
 const options = { 
-  all: { type: "boolean", short: "a"},
-  help: { type: "boolean", short: "h" } 
+	project: { type: "string", short: "p"},
+  help: { type: "boolean", short: "h" },
+	frontend: { type: "string", short: 'f'},
+	backend: { type: "string", short: 'b'}
 }
 const { values, positionals } = parseArgs({options, strict: false })
 
