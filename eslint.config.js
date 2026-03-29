7149df517e0ca5ad6ac85429eae5d30e518b5315
Santiago <fsgreco@fastmail.com>
2026-03-30 00:40:27 +0200

    Update eslint config for v10

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,20 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
 import globals from 'globals'
-import pluginJs from '@eslint/js'
+import js from '@eslint/js'
 import eslintConfigPrettier from 'eslint-config-prettier'
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
-	{ languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-	pluginJs.configs.recommended,
+export default defineConfig([
+	globalIgnores(['.github/**', 'dist/**', 'public/**', '**/playwright-report']),
 	{
-		ignores: ['node_modules', '.github', 'dist', 'public'],
+		files: ['**/*.{js,mjs,cjs,ts,tsx}'],
+		plugins: { js },
+		extends: ['js/recommended', eslintConfigPrettier],
+		languageOptions: { globals: { ...globals.browser, ...globals.node } },
 		rules: {
 			'no-unused-vars': 'warn',
 			'no-useless-escape': 'off',
 			'prefer-const': 'off',
 		},
 	},
-	eslintConfigPrettier,
-]
+])
